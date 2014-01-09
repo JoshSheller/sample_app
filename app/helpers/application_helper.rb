cd10504a216c7a0a8362d13e90fdb505b1646d9f
Joshua Sheller <joshua.a.sheller@gmail.com>
2014-01-08 22:08:19 -0600

    cleaning up old waste

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Neat Nest Maid Service"
+    base_title = "Sheller Coding"
     if page_title.empty?
       base_title
     else
